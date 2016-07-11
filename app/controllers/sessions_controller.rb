9c356d09ab3eab98866a8469aa616a8a1271d083
App Academy Student <appacademy@Apps-Mac-mini-92.local>
2016-07-11 09:48:55 -0400

    more controller work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_credentials(
+    user = User.find_by_credentials( #calling this will ensure_session_token (need after_initialize)
         params[:user][:user_name],
         params[:user][:password]) # params seems to be really powerful http://guides.rubyonrails.org/action_controller_overview.html
     if user.nil?
@@ -20,9 +20,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    logout_user! # different from login_user 
+    logout_user! # different from login_user
     #redirect_to new_session_url
-
   end
 
 
